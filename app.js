590bcfeb9a9a02cc0a585c9b5309856c2ae7ce51
sabrina-boby <sabrinaboby786@gmail.com>
2024-11-02 22:17:54 +0600

    update

--- app.js
+++ app.js
@@ -5,30 +5,26 @@ import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./error/error.js";
 import reservationRouter from './routes/reservationRoute.js';
 
-
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["POST"],
-//     credentials: true,
-//   })
-// );
+// Updated CORS configuration to allow multiple origins
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["POST","GET"], // add any additional methods you need
+    origin: [process.env.FRONTEND_URL, "https://restaurants-ui.vercel.app"], // Add both local and Vercel URLs
+    methods: ["POST", "GET"], // Add any additional methods you need
     credentials: true,
   })
 );
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/v1/reservation',reservationRouter)
+app.use('/api/v1/reservation', reservationRouter);
 
 dbConnection();
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 export default app;
+
+
+
+
